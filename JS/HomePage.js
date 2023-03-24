350c98eab82d053d6d9d8fd62c6e2a0c6a1e0a66
varsha Gunjal <varshashingote01@gmail.com>
2023-03-24 14:40:41 +0530

    [varsha] ViewEmployeePayRollDetails

--- JS/HomePage.js
+++ JS/HomePage.js
@@ -1,5 +1,5 @@
 /* UC5- Display Employee Details from JSON Object */
- window.addEventListener("DOMContentLoaded", (event) => {
+/*window.addEventListener("DOMContentLoaded", (event) => {
     createInnerHtml();
  });
  const createInnerHtml=() => {
@@ -74,4 +74,49 @@
         deptHtml=`${deptHtml}<div class='dept-label'>${dept}</div>`
      }
      return deptHtml;
-   }
+   }-->
+
+   /* UC6- Display Employee Details from Local Storage*/
+
+let empPayrollList;
+window.addEventListener("DOMContentLoaded", (event) => {
+    empPayrollList = getEmployeePayrollDataStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
+    createInnerHtml();
+    localStorage.removeItem('editEmp');
+ });
+ const getEmployeePayrollDataStorage =() => {
+    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+ }
+
+ const createInnerHtml=() => {
+    const headerHtml="<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>StartDate</th><th>Actions</th>";
+   if(empPayrollList.length == 0) return;
+    let innerHtml=`${headerHtml}`;
+    for( const empPayrollData of empPayrollList){
+     innerHtml =`${innerHtml}
+     <tr>
+     <td><img class="profile" alt="" src="${empPayrollData._profilePic}">
+     </td>
+     <td>${empPayrollData._name}</td>
+     <td>${empPayrollData._gender}</td>
+     <td>${getDeptHtml(empPayrollData._department)}</td>
+     <td>${empPayrollData._salary}</td>
+     <td>${empPayrollData._startDate}</td>
+     <td>
+         <img id="${empPayrollData._id}" onclick="remove(this)" alt="delete" src="../assert/icon/delete-black-18dp.svg">
+         <img id="${empPayrollData._id}" onclick="update(this)" alt="edit" src="../assert/icons/create-black-18dp.svg">
+     </td>
+ </tr>
+`;
+}
+document.querySelector('#table-display').innerHTML=innerHtml;
+}
+const getDeptHtml=(deptList)=>
+{
+  let deptHtml='';
+  for(const dept of deptList){
+     deptHtml=`${deptHtml}<div class='dept-label'>${dept}</div>`
+  }
+  return deptHtml;
+}
